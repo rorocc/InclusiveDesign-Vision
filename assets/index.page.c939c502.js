6ab1ff4da3cc8b2c57dadf91a91be772c9f6bf01
jacobmellin <jacob.mellin@gmail.com>
2022-07-04 10:39:32 +0000

    Deploying to gh-pages from @ rorocc/InclusiveDesign-Vision@06225c4b04dc1a5ea63fb6f9c2c7d230110da3c6 🚀

--- assets/index.page.c939c502.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as a,o as t,F as c,a as e}from"./renderer/_default.page.client.ts.89ccde75.js";const n={},r=e("h1",null,"About",-1),s=e("p",null,"A colored page.",-1);function _(l,d){return t(),a(c,null,[r,s],64)}var u=o(n,[["render",_]]);export{u as default};
